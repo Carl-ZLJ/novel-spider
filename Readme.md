0888ab035cab540d8f014f4ecacc39e8af187292
carlz32 <zljmms32@gmail.com>
2024-10-17 11:06:01 +0800

    add formatter(prettier)

--- Readme.md
+++ Readme.md
@@ -1,9 +1,11 @@
 # Novel Spider
 
 ## Description
-use [Puppeteer](https://pptr.dev/) to download online novels and save as __txt__ file
+
+use [Puppeteer](https://pptr.dev/) to download online novels and save as **txt** file
 
 ## How to use?
+
 - Download this repo
 - Run `npm install`
 - Invoke the `download(url)` function with the url of the novel
--- processUI.js
+++ processUI.js
@@ -1,6 +1,8 @@
-export const printProgress = function(current, total) {
+export const printProgress = function (current, total) {
     process.stdout.clearLine()
     process.stdout.cursorTo(0)
 
-    process.stdout.write(`Progress: ${current}/${total} (${(current / total * 100).toFixed(2)}%)`)
+    process.stdout.write(
+        `Progress: ${current}/${total} (${((current / total) * 100).toFixed(2)}%)`
+    )
 }
