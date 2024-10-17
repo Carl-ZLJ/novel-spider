382fcdd1910f8720228c966be6fc35766e1286f1
carlz32 <zljmms32@gmail.com>
2024-10-17 10:12:58 +0800

    add prettier config

--- main.js
+++ main.js
@@ -2,23 +2,25 @@ import fs from 'node:fs'
 import puppeteer from 'puppeteer'
 import asyncPool from './pool.js'
 
-const log = function() {
+const log = function () {
     console.log.apply(console, arguments)
 }
 
 function charpterFromLink(browser) {
-    return async function(link) {
-
+    return async function (link) {
         const { url, charpterIndex } = link
         const page = await browser.newPage()
         await page.goto(url, {
             waitUntil: 'domcontentloaded',
         })
-        await page.waitForSelector('#content') 
-        const title = await page.$eval('#content h1', title => title.innerText)
-        const content = await page.$eval('#content', content => {
+        await page.waitForSelector('#content')
+        const title = await page.$eval(
+            '#content h1',
+            (title) => title.innerText
+        )
+        const content = await page.$eval('#content', (content) => {
             const nodes = Array.from(content.childNodes)
-            return nodes.map(n => n.nodeType == 3 ? n.textContent : '')
+            return nodes.map((n) => (n.nodeType == 3 ? n.textContent : ''))
         })
         page.close()
 
@@ -32,24 +34,28 @@ function charpterFromLink(browser) {
 }
 
 function charpterText(title, content) {
-    content = content.map(line => line.trimStart())
+    content = content.map((line) => line.trimStart())
 
     return `${title.split(' ')[1]}\n${content.slice(10).join('\n')}\n`
 }
 
 async function linksFromUrl(browser, url) {
     const page = await browser.newPage()
-    
+
     await page.goto(url, {
         waitUntil: 'domcontentloaded',
     })
-    await page.waitForSelector('div.centent') 
-    const links = await page.$$eval('div.centent ul li a', (ls, baseUrl) => {
-        return ls.map((l, index) => ({
-            url: baseUrl + l.getAttribute('href'),
-            charpterIndex: index,
-        }))
-    }, url)
+    await page.waitForSelector('div.centent')
+    const links = await page.$$eval(
+        'div.centent ul li a',
+        (ls, baseUrl) => {
+            return ls.map((l, index) => ({
+                url: baseUrl + l.getAttribute('href'),
+                charpterIndex: index,
+            }))
+        },
+        url
+    )
 
     page.close()
     return links
@@ -62,9 +68,13 @@ async function download(url, concurrency = 20) {
 
     const charpters = []
 
-    log('Downloading html content ...')    
-    for await (const value of asyncPool(concurrency, links, charpterFromLink(browser))) {
-        const { charpter, charpterIndex } = value        
+    log('Downloading html content ...')
+    for await (const value of asyncPool(
+        concurrency,
+        links,
+        charpterFromLink(browser)
+    )) {
+        const { charpter, charpterIndex } = value
         charpters[charpterIndex] = charpter
     }
 
@@ -76,7 +86,7 @@ async function download(url, concurrency = 20) {
     try {
         fs.writeFileSync('轮回乐园.txt', charpters.join('\n'))
     } catch (error) {
-        throw new Error(`Failed to write!!!`)        
+        throw new Error(`Failed to write!!!`)
     }
     log('Finished writing...')
 }
